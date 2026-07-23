2ce8c2b0d7c519d3b80df8c8294a5ecc7ea35762
vaishnavi5640 <vaishnavi5640@tsecmumbai.in>
2026-07-23 20:25:56 +0530

    Added AnkVidya branding and custom logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,21 @@
+import logo from "../images/logo/logo.png";
+
 function Navbar() {
   return (
     <nav className="navbar">
-      <h2 className="logo">AnkVidya</h2>
+      <div className="brand">
+        <img src={logo} alt="AnkVidya Logo" className="logo-image" />
+
+        <div className="brand-text">
+          <h2 className="logo">AnkVidya</h2>
+          <p className="tagline">Ancient Knowledge • Modern Learning</p>
+        </div>
+      </div>
 
       <ul className="nav-links">
         <li>Home</li>
         <li>Learn</li>
-        <li>Number Explorer</li>
+        <li>Explorer</li>
         <li>AI Tutor</li>
         <li>Quiz</li>
       </ul>
